c13ed552fd622d842d21e4b9fd6d74d6e674eae5
Airish <airish7x22@gmail.com>
2026-08-14 04:57:21 +0530

    Deploy full vector SVG logos and colorful category badges across all 308 AI tools

---
+++ _next/static/pOYvJ1XTnRtUflkTeTT7X/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/WAMA.ai//_next/:path+",
+        "destination": "/WAMA.ai/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
